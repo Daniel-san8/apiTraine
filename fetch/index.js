431d81abf0d7358181005739aed65902d4d33f7e
Daniel San <danielgdt88@hotmail.com>
2024-06-14 20:51:31 -0300

    json metodo

--- fetch/index.js
+++ fetch/index.js
@@ -7,6 +7,15 @@ doc
     return r.text();
   })
   .then((r) => {
-    document.body.innerHTML = r;
-    console.log(r);
+    // document.body.innerHTML = r;
+  });
+
+const apiFetch = fetch("https://viacep.com.br/ws/40350000/json/");
+
+apiFetch
+  .then((r) => {
+    return r.json();
+  })
+  .then((r) => {
+    document.body.innerText += r.uf;
   });
