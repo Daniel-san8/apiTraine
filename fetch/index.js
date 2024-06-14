d556d9c3e27bdb453d73fd2b83ab85b7ceefcd4e
Daniel San <danielgdt88@hotmail.com>
2024-06-14 20:03:01 -0300

    testando fetch

--- fetch/index.js
+++ fetch/index.js
@@ -1,3 +1,12 @@
 const doc = fetch("./dox.txt");
 
 console.log(doc);
+
+doc
+  .then((r) => {
+    return r.text();
+  })
+  .then((r) => {
+    document.body.innerHTML = r;
+    console.log(r);
+  });
