5d3d5fa58be04f2d9bf9666a4d18364c79f865e8
Daniel San <danielgdt88@hotmail.com>
2024-06-14 19:43:45 -0300

    race

--- apiAdvanced/index.js
+++ apiAdvanced/index.js
@@ -1,11 +1,23 @@
 const login = new Promise((resolve) => {
   setTimeout(() => {
     resolve("Usuário logado");
-  });
+  }, 1000);
 });
 
 const dados = new Promise((resolve) => {
   setTimeout(() => {
     resolve("Dados carregados");
-  });
+  }, 500);
+});
+
+const carregouTudo = Promise.all([login, dados]);
+
+carregouTudo.then((resolve) => {
+  console.log(resolve);
+});
+
+const carregouTudoRace = Promise.race([login, dados]);
+
+carregouTudoRace.then((res) => {
+  console.log(res);
 });
