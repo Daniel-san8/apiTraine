23e9ad73d42543c26593fb5880bc5d313c4cefbc
Daniel San <danielgdt88@hotmail.com>
2024-06-14 21:21:56 -0300

    usando blob

--- blob/index.js
+++ blob/index.js
@@ -10,3 +10,13 @@ blobImg
     img.setAttribute("src", urlR);
     document.body.appendChild(img);
   });
+
+const api = fetch("https://viacep.com.br/ws/40350000/json/");
+
+api
+  .then((r) => {
+    return r.text();
+  })
+  .then((r) => {
+    console.log(r);
+  });
