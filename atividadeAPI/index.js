437dec56b8ee338c364539f9b22ff77009659267
Daniel San <danielgdt88@hotmail.com>
2024-06-14 22:00:22 -0300

    atividade 1 feita

--- atividadeAPI/index.js
+++ atividadeAPI/index.js
@@ -1,1 +1,15 @@
-const cepApi = fetch(`https://viacep.com.br/ws/${inputCep}/json/`);
+const botao = document.querySelector("[data-botao]");
+botao.addEventListener("click", (e) => {
+  e.preventDefault();
+  inputCep = document.querySelector("[data-cep]").value;
+  let cepApi = fetch(`https://viacep.com.br/ws/${inputCep}/json/`);
+  cepApi
+    .then((r) => {
+      return r.text();
+    })
+    .then((r) => {
+      let divZada = document.createElement("div");
+      divZada.innerHTML = r;
+      document.body.appendChild(divZada);
+    });
+});
