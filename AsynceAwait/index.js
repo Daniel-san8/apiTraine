6ce20f581b869f5636aec5c84003600ad9a9d6a7
Daniel San <danielgdt88@hotmail.com>
2024-06-16 10:39:26 -0300

    subindo arquivos async e await

--- AsynceAwait/index.js
+++ AsynceAwait/index.js
@@ -1,7 +1,21 @@
 async function puxarDados() {
-  const responseDados = await fetch("./dados.json");
-  const jsonDados = await responseDados.json();
-  console.log(jsonDados);
+  try {
+    const responseDados = await fetch("./dados.json");
+    const jsonDados = await responseDados.json();
+  } catch (e) {
+    console.log(`o erro foi: ${e}`);
+  }
 }
 
-puxarDados();
+async function puxarDados2() {
+  const dados = fetch("./dados.json");
+  const clientes = fetch("./clientes.json");
+  const responseDados = await dados;
+  const resolvidoPrimeiro = await (await responseDados).json();
+  const clientesDados = await clientes;
+  const clientesDadosPrimeiro = await (await clientesDados).json();
+  console.log(resolvidoPrimeiro);
+  console.log(clientesDadosPrimeiro);
+}
+
+puxarDados2();
