8167ac3ca741e96c50412f0af8e65132f2dc738d
Daniel San <danielgdt88@hotmail.com>
2024-06-16 11:10:02 -0300

    metodos de history

--- AsynceAwait/history/index.js
+++ AsynceAwait/history/index.js
@@ -9,11 +9,25 @@ const links = document.querySelectorAll("a");
 function handleClick(event) {
   event.preventDefault();
   fetchPage(event.target.href);
+  window.history.pushState(null, null, event.target.href);
 }
 
-function fetchPage(url) {
-  console.log(url);
+async function fetchPage(url) {
+  const pageResponse = await fetch(url);
+  const pageResponseText = await pageResponse.text();
+  replaceContent("Daniel é top");
 }
+
+function replaceContent(newtext) {
+  const newHtml = document.createElement("div");
+  newHtml.innerHTML = newtext;
+  const oldContent = document.querySelector(".content");
+  oldContent.innerHTML = newtext;
+}
+
+window.addEventListener("popstate", () => {
+  fetchPage(window.location.href);
+});
 
 links.forEach((link) => {
   link.addEventListener("click", handleClick);
