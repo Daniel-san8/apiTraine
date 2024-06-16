d4a196ba2831f20a1b1787f9b3c08c69c6a60b4a
Daniel San <danielgdt88@hotmail.com>
2024-06-16 10:57:55 -0300

    metodos de history

--- AsynceAwait/history/index.js
+++ AsynceAwait/history/index.js
@@ -1,5 +1,20 @@
-window.history.pushState(null, null, "OPA");
+// window.history.pushState(null, null, "sobre.html");
 
-window.addEventListener("popstate", () => {
-  console.log("teste");
+// window.addEventListener("popstate", () => {
+//   console.log("teste");
+// });
+
+const links = document.querySelectorAll("a");
+
+function handleClick(event) {
+  event.preventDefault();
+  fetchPage(event.target.href);
+}
+
+function fetchPage(url) {
+  console.log(url);
+}
+
+links.forEach((link) => {
+  link.addEventListener("click", handleClick);
 });
